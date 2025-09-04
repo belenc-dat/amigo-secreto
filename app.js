e59a36ab98d6356bd0ea09cfc1172002ca2e9d57
Belenc <belentaywara@gmail.com>
2025-09-04 01:27:30 -0500

    Act agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let amigosArr = [];
+
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo');
+    
+    if (inputAmigo.trim() === ''){
+        alert('Inserte un nombre, por favor');
+    } else {
+        //agregar nombre
+        amigosArr.push(inputAmigo.value);
+        //limpiar
+        document.querySelector('#amigo').value = '';
+        verListaAmigos();
+    }
+    return;
+}
+
+
+
